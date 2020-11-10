3fcee6eca551c76aeb617729a0b0a11906f84f1a
LA - TA MINH KHA <tmk001-dn-la@fhmc.com>
2020-11-10 17:01:09 +0700

    up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://www.youtube.com/watch?v=fACSczNshtI&t=2635s
+pass db mongodb
+adminos
+ Eg9XwgCOQAI18fwR
--- index.js
+++ index.js
@@ -5,10 +5,23 @@ app.set("view engine","ejs");
 app.set("views","./views");
 app.use(express.static("public"));
 app.listen(3000);
+//mongoose
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://adminos :v16vY9s8my3JgABN@cluster0.rwxhd.mongodb.net/Marvels?retryWrites=true&w=majority',
+ {useNewUrlParser: true, useUnifiedTopology: true},function(err){
+    if(err){
+        console.log(new(Date)+"mongoose connect err "+ err);
+    }else{
+        console.log("Connect ok")
+    }}
+);
+//
 //body-parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//
 //multer
 var multer  = require('multer');
 var storage = multer.diskStorage({
@@ -48,9 +61,11 @@ app.post("/add",function(req,res){
    
     upload(req, res, function (err) {
         if (err instanceof multer.MulterError) {
-          console.log("A Multer error occurred when uploading."); 
+          //console.log("A Multer error occurred when uploading."); 
+          res.json({"kq":0,"errMsg":" Multer error occurred when uploading"});
         } else if (err) {
-          console.log("An unknown error occurred when uploading." + err);
+          //console.log("An unknown error occurred when uploading." + err);
+          res.json({"kq":0,"errMsg":" unknown error occurred when uploading"});
         }else{
             // không có lỗi
             console.log("Upload is okay");
@@ -63,6 +78,19 @@ app.post("/add",function(req,res){
             //     res.json({"result":0});
             // }
             res.send(req.file.filename);
+            var marvel=Marvel({
+                Name:req.body.txtName,
+                Image:req.file.filename,
+                Level:req.body.txtLevel
+            });
+            marvel.save((err)=>{
+                if (err){
+                    res.json({"kq":0,"errMsg":err});
+                }else{
+                    res.json({"kq":1});
+                }
+                
+            })
         }
 
     });
